bcef4b9b46a5abc19789996cbaa167a403dd395d
Dushant <dushantkumar172@gmail.com>
2020-05-07 10:15:23 -0700

    first commit of project

---
+++ HR data vilz with python.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  7 09:55:54 2020
+
+@author: jethi
+"""
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import seaborn as sns
+
+""" Data upload """
+
+Data= pd.read_csv('HRDataset_v13.csv')
+
+
+
+
